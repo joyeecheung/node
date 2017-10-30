47a686bbc33c1d0ac48aa77a88c5c9963ecab13e
Joyee Cheung <joyeec9h3@gmail.com>
2017-10-30 09:14:29 +0800

    test: fix flayky test-async-wrap-uncaughtexception

--- test/parallel/test-async-wrap-uncaughtexception.js
+++ test/parallel/test-async-wrap-uncaughtexception.js
@@ -7,13 +7,14 @@ if (!common.hasCrypto)
 const assert = require('assert');
 const async_hooks = require('async_hooks');
 const call_log = [0, 0, 0, 0];  // [before, callback, exception, after];
+const [BEFORE, CALLBACK, EXCEPTION, AFTER] = [0, 1, 2, 3];
 let call_id = null;
 let hooks = null;
 
 
-process.on('beforeExit', common.mustCall(() => {
+process.on('beforeExit', common.mustCall((code) => {
+  console.log(`beforeExit fired with exit code ${code}`);
   process.removeAllListeners('uncaughtException');
-  hooks.disable();
   assert.strictEqual(typeof call_id, 'number');
   assert.deepStrictEqual(call_log, [1, 1, 1, 1]);
 }));
@@ -25,22 +26,23 @@ hooks = async_hooks.createHook({
       call_id = id;
   },
   before(id) {
-    if (id === call_id) call_log[0]++;
+    if (id === call_id) call_log[BEFORE]++;
   },
   after(id) {
-    if (id === call_id) call_log[3]++;
+    if (id === call_id) call_log[AFTER]++;
+    hooks.disable();
   },
 }).enable();
 
 
 process.on('uncaughtException', common.mustCall(() => {
   assert.strictEqual(call_id, async_hooks.executionAsyncId());
-  call_log[2]++;
+  call_log[EXCEPTION]++;
 }));
 
 
 require('crypto').randomBytes(1, common.mustCall(() => {
   assert.strictEqual(call_id, async_hooks.executionAsyncId());
-  call_log[1]++;
+  call_log[CALLBACK]++;
   throw new Error('ah crap');
 }));
