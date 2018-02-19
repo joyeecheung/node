cf0d2de6bc1bcfe96d74820302d616e843325118
Joyee Cheung <joyeec9h3@gmail.com>
2018-02-20 02:43:16 +0800

    fs: throw fs.fchmodSync errors in JS land

--- test/parallel/test-fs-error-messages.js
+++ test/parallel/test-fs-error-messages.js
@@ -562,3 +562,23 @@ function re(literals, ...values) {
     );
   });
 }
+
+// fchmod
+{
+  const validateError = (err) => {
+    assert.strictEqual(err.message, 'EBADF: bad file descriptor, fchmod');
+    assert.strictEqual(err.errno, uv.UV_EBADF);
+    assert.strictEqual(err.code, 'EBADF');
+    assert.strictEqual(err.syscall, 'fchmod');
+    return true;
+  };
+
+  common.runWithInvalidFD((fd) => {
+    fs.fchmod(fd, 0o666, common.mustCall(validateError));
+
+    assert.throws(
+      () => fs.fchmodSync(fd, 0o666),
+      validateError
+    );
+  });
+}
