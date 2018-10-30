f31544207e5779386871736af0fc7b7118684ddd
Joyee Cheung <joyeec9h3@gmail.com>
2018-10-30 23:24:14 +0800

    test: test all cacheable modules in test-code-cache

--- test/code-cache/test-code-cache.js
+++ test/code-cache/test-code-cache.js
@@ -14,10 +14,17 @@ const {
 } = require('util');
 const {
   cachableBuiltins,
+  cannotUseCache,
   codeCache,
   compiledWithCache,
   compiledWithoutCache
 } = require('internal/bootstrap/cache');
+
+for (const key of cachableBuiltins) {
+  if (!cannotUseCache.includes(key)) {
+    require(key);
+  }
+}
 
 const loadedModules = process.moduleLoadList
   .filter((m) => m.startsWith('NativeModule'))
@@ -26,6 +33,7 @@ const loadedModules = process.moduleLoadList
 // The binary is not configured with code cache, verifies that the builtins
 // are all compiled without cache and we are doing the bookkeeping right.
 if (process.config.variables.node_code_cache_path === undefined) {
+  console.log('The binary is not configured with code cache');
   assert.deepStrictEqual(compiledWithCache, []);
   assert.notStrictEqual(compiledWithoutCache.length, 0);
 
@@ -33,22 +41,28 @@ if (process.config.variables.node_code_cache_path === undefined) {
     assert(compiledWithoutCache.includes(key),
            `"${key}" should not have been compiled with code cache`);
   }
-
 } else {
-  // The binary is configured with code cache.
+  console.log('The binary is configured with code cache');
   assert.strictEqual(
     typeof process.config.variables.node_code_cache_path,
     'string'
   );
-  assert.deepStrictEqual(compiledWithoutCache, []);
+
+  for (const key of compiledWithoutCache) {
+    assert.ok(cannotUseCache.includes(key));
+  }
 
   for (const key of loadedModules) {
-    assert(compiledWithCache.includes(key),
-           `"${key}" should've been compiled with code cache`);
+    if (!cannotUseCache.includes(key)) {
+      assert(compiledWithCache.includes(key),
+            `"${key}" should've been compiled with code cache`);
+    }
   }
 
   for (const key of cachableBuiltins) {
-    assert(isUint8Array(codeCache[key]) && codeCache[key].length > 0,
-           `Code cache for "${key}" should've been generated`);
+    if (!cannotUseCache.includes(key)) {
+      assert(isUint8Array(codeCache[key]) && codeCache[key].length > 0,
+             `Code cache for "${key}" should've been generated`);
+    }
   }
 }
