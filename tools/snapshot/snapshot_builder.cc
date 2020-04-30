d0d7b67ec30c8eb9fe596e278108cfac5bdeed81
Joyee Cheung <joyeec9h3@gmail.com>
2020-04-30 16:03:59 +0800

    tools: re-enable CanBeRehashed() check

--- tools/snapshot/snapshot_builder.cc
+++ tools/snapshot/snapshot_builder.cc
@@ -144,7 +144,7 @@ std::string SnapshotBuilder::Generate(
     // Must be out of HandleScope
     StartupData blob =
         creator.CreateBlob(SnapshotCreator::FunctionCodeHandling::kClear);
-    // CHECK(blob.CanBeRehashed());
+    CHECK(blob.CanBeRehashed());
     // Must be done while the snapshot creator isolate is entered i.e. the
     // creator is still alive.
     FreeEnvironment(env);
