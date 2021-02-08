7517725500b17e055312a31f905cc34d5468bdd5
Joyee Cheung <joyeec9h3@gmail.com>
2021-02-08 20:34:02 +0800

    src: support serialization of binding data
    This patch adds the SnapshotableObject interface. Native objects
    supporting serialization can inherit from it, implementing
    PrepareForSerialization(), Serialize() and Deserialize()
    to control how the native states should be serialized and
    deserialized.
    
    See doc: https://docs.google.com/document/d/15bu038I36oILq5t4Qju1sS2nKudVB6NSGWz00oD48Q8/edit

--- src/node_snapshotable.cc
+++ src/node_snapshotable.cc
@@ -1,39 +1,148 @@
 
 #include "node_snapshotable.h"
 #include "base_object-inl.h"
+#include "debug_utils-inl.h"
+#include "node_file.h"
+#include "node_v8.h"
 
 namespace node {
 
 using v8::Local;
 using v8::Object;
+using v8::SnapshotCreator;
 using v8::StartupData;
+
+SnapshotableObject::SnapshotableObject(Environment* env,
+                                       Local<Object> wrap,
+                                       EmbedderObjectType type)
+    : BaseObject(env, wrap), type_(type) {
+  set_is_snapshotable(true);
+}
+
+const char* SnapshotableObject::GetTypeNameChars() const {
+  switch (type_) {
+#define V(PropertyName, NativeTypeName)                                        \
+  case EmbedderObjectType::k_##PropertyName: {                                 \
+    return NativeTypeName::type_name.c_str();                                  \
+  }
+    SERIALIZABLE_OBJECT_TYPES(V)
+#undef V
+    default: { UNREACHABLE(); }
+  }
+}
+
+bool IsSnapshotableType(FastStringKey key) {
+#define V(PropertyName, NativeTypeName)                                        \
+  if (key == NativeTypeName::type_name) {                                      \
+    return true;                                                               \
+  }
+  SERIALIZABLE_OBJECT_TYPES(V)
+#undef V
+
+  return false;
+}
 
 void DeserializeNodeInternalFields(Local<Object> holder,
                                    int index,
-                                   v8::StartupData payload,
+                                   StartupData payload,
                                    void* env) {
+  per_process::Debug(DebugCategory::MKSNAPSHOT,
+                     "Deserialize internal field %d of %p, size=%d\n",
+                     static_cast<int>(index),
+                     (*holder),
+                     static_cast<int>(payload.raw_size));
   if (payload.raw_size == 0) {
     holder->SetAlignedPointerInInternalField(index, nullptr);
     return;
   }
-  // No embedder object in the builtin snapshot yet.
-  UNREACHABLE();
+
+  // TODO(joyeecheung): here we assume that
+  // 1. the holder is a BaseObject
+  // 2. the slot is the and BaseObject::kSlot and we are reviving
+  //    the BaseObject reference
+  // 3. the payload contains a InternalFieldInfo whose type indicates
+  //    the type of the BaseObject.
+  // Which may be expanded if we want to support non-BaseObject types
+  // or types with more slots.
+  CHECK_EQ(index, BaseObject::kSlot);
+  Environment* env_ptr = static_cast<Environment*>(env);
+  const InternalFieldInfo* info =
+      reinterpret_cast<const InternalFieldInfo*>(payload.data);
+
+  switch (info->type) {
+#define V(PropertyName, NativeTypeName)                                        \
+  case EmbedderObjectType::k_##PropertyName: {                                 \
+    per_process::Debug(DebugCategory::MKSNAPSHOT,                              \
+                       "Object %p is %s\n",                                    \
+                       (*holder),                                              \
+                       NativeTypeName::type_name.c_str());                     \
+    env_ptr->EnqueueDeserializeRequest(                                        \
+        NativeTypeName::Deserialize, holder, info->Copy());                    \
+    break;                                                                     \
+  }
+    SERIALIZABLE_OBJECT_TYPES(V)
+#undef V
+    default: { UNREACHABLE(); }
+  }
 }
 
 StartupData SerializeNodeContextInternalFields(Local<Object> holder,
                                                int index,
                                                void* env) {
+  per_process::Debug(DebugCategory::MKSNAPSHOT,
+                     "Serialize internal field, index=%d, holder=%p\n",
+                     static_cast<int>(index),
+                     *holder);
   void* ptr = holder->GetAlignedPointerFromInternalField(index);
-  if (ptr == nullptr || ptr == env) {
-    return StartupData{nullptr, 0};
-  }
-  if (ptr == env && index == ContextEmbedderIndex::kEnvironment) {
+  if (ptr == nullptr) {
     return StartupData{nullptr, 0};
   }
 
-  // No embedder objects in the builtin snapshot yet.
-  UNREACHABLE();
-  return StartupData{nullptr, 0};
+  // For now, we just assume that we can only deal with kSlot fields of
+  // BaseObjects.
+  // TODO(joyeecheung): add more types for other objects with embedder fields.
+  CHECK_EQ(index, BaseObject::kSlot);
+
+  DCHECK(static_cast<BaseObject*>(ptr)->is_snapshotable());
+  SnapshotableObject* obj = static_cast<SnapshotableObject*>(ptr);
+  per_process::Debug(DebugCategory::MKSNAPSHOT,
+                     "Object %p is %s, ",
+                     *holder,
+                     obj->GetTypeNameChars());
+  InternalFieldInfo* info = obj->Serialize();
+  per_process::Debug(DebugCategory::MKSNAPSHOT,
+                     "payload size=%d\n",
+                     static_cast<int>(info->length));
+  return StartupData{reinterpret_cast<const char*>(info),
+                     static_cast<int>(info->length)};
+}
+
+void SerializeBindingData(Environment* env,
+                          SnapshotCreator* creator,
+                          EnvSerializeInfo* info) {
+  size_t i = 0;
+  env->ForEachBindingData([&](FastStringKey key,
+                              BaseObjectPtr<BaseObject> binding) {
+    per_process::Debug(DebugCategory::MKSNAPSHOT,
+                       "Serialize binding %i, %p, type=%s\n",
+                       static_cast<int>(i),
+                       *(binding->object()),
+                       key.c_str());
+
+    if (IsSnapshotableType(key)) {
+      size_t index = creator->AddData(env->context(), binding->object());
+      per_process::Debug(DebugCategory::MKSNAPSHOT,
+                         "Serialized with index=%d\n",
+                         static_cast<int>(index));
+      info->bindings.push_back({key.c_str(), i, index});
+      SnapshotableObject* ptr = static_cast<SnapshotableObject*>(binding.get());
+      ptr->PrepareForSerialization(env->context(), creator);
+    } else {
+      UNREACHABLE();
+    }
+
+    i++;
+  });
 }
 
 }  // namespace node
