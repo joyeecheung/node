e4d3d3c77e74ca9b28188b5a4e470b107c0dc058
Joyee Cheung <joyeec9h3@gmail.com>
2021-09-07 02:44:14 +0800

    support gzlib in snapshot

---
+++ test/fixtures/snapshot/decompress-gzip.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const zlib = require('zlib');
+const fs = require('fs');
+const assert = require('assert');
+
+const fixture = process.env.NODE_TEST_FIXTURE;
+const mode = process.env.NODE_TEST_MODE;
+const gunzip = zlib.createGunzip();
+const inp = fs.createReadStream(fixture);
+
+inp.pipe(gunzip);
+
+let data = [];
+gunzip.on('data', (d) => {
+  console.log(`Pushing ${gunzip.bytesWritten} bytes into the `+
+              `#${data.length} chunk`);
+  data.push(d);
+});
+gunzip.on('end', (d) => {
+  const result = Buffer.concat(data);
+  console.log(`Result length = ${result.byteLength}`);
+  if (mode === 'snapshot') {
+    globalThis.NODE_TEST_DATA = result;
+  } else {
+    assert.deepStrictEqual(globalThis.NODE_TEST_DATA, result);
+  }
+  console.log('NODE_TEST_MODE:', mode);
+});
