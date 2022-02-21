dfa35178113ae920ba71912d2224f1bf8edd6381
Joyee Cheung <joyeec9h3@gmail.com>
2022-02-22 03:15:47 +0800

    test: add memory bug tests

---
+++ test/parallel/test-snapshot-mem-bug.js
@@ -0,0 +1,93 @@
+'use strict';
+
+// https://bugs.chromium.org/p/v8/issues/detail?id=12718
+
+const common = require('../common');
+const assert = require('assert');
+const { spawnSync } = require('child_process');
+const tmpdir = require('../common/tmpdir');
+const fixtures = require('../common/fixtures');
+const path = require('path');
+const fs = require('fs');
+
+tmpdir.refresh();
+const blobPath = path.join(tmpdir.path, 'snapshot.blob');
+const empty = fixtures.path('empty.js');
+
+{
+  // Create the snapshot.
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    '--build-snapshot',
+    fixtures.path('snapshot', 'mem-bug.js'),
+  ], {
+    cwd: tmpdir.path
+  });
+  if (child.status !== 0) {
+    console.log(child.stderr.toString());
+    console.log(child.stdout.toString());
+    assert.strictEqual(child.status, 0);
+  }
+  const stats = fs.statSync(blobPath);
+  assert(stats.isFile());
+}
+
+{
+  // Check snapshot
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    empty,
+  ], {
+    cwd: tmpdir.path
+  });
+
+  if (child.status !== 0) {
+    console.log(child.stderr.toString());
+    console.log(child.stdout.toString());
+    if (!common.isWindows)
+      console.log('Signal', child.signal);
+    assert.strictEqual(child.status, 0);
+  }
+}
+
+tmpdir.refresh();
+
+{
+  // Create the snapshot.
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    '--build-snapshot',
+    fixtures.path('snapshot', 'mem-bug2.js'),
+  ], {
+    cwd: tmpdir.path
+  });
+  if (child.status !== 0) {
+    console.log(child.stderr.toString());
+    console.log(child.stdout.toString());
+    assert.strictEqual(child.status, 0);
+  }
+  const stats = fs.statSync(blobPath);
+  assert(stats.isFile());
+}
+
+{
+  // Check snapshot
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    empty,
+  ], {
+    cwd: tmpdir.path
+  });
+
+  if (child.status !== 0) {
+    console.log(child.stderr.toString());
+    console.log(child.stdout.toString());
+    if (!common.isWindows)
+      console.log('Signal', child.signal);
+    assert.strictEqual(child.status, 0);
+  }
+}
