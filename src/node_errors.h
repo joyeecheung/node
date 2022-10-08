679b284e30d19c7a946db1e30613b45f963766df
legendecas <legendecas@gmail.com>
2022-10-08 16:54:50 +0800

    module: migrate to script context based host defined options
    This allows the host-defined options to be an arbitrary JavaScript value
    so that the module/script wrapper object can be used as the host-defined
    options instead. In this way, we can move away from the id-based module
    tables and fixes the problem that the scripts can out lives the module
    wrapper objects.

--- src/node_errors.h
+++ src/node_errors.h
@@ -69,6 +69,7 @@ void OOMErrorHandler(const char* location, const v8::OOMDetails& details);
   V(ERR_INVALID_OBJECT_DEFINE_PROPERTY, TypeError)                             \
   V(ERR_INVALID_MODULE, Error)                                                 \
   V(ERR_INVALID_STATE, Error)                                                  \
+  V(ERR_INVALID_SCRIPT_CONTEXT, TypeError)                                     \
   V(ERR_INVALID_THIS, TypeError)                                               \
   V(ERR_INVALID_TRANSFER_OBJECT, TypeError)                                    \
   V(ERR_MEMORY_ALLOCATION_FAILED, Error)                                       \
