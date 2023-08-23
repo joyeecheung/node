7779fabb4ba5a1b3ed341ce110268ef450e55d0f
Joyee Cheung <joyeec9h3@gmail.com>
2023-08-23 18:57:08 +0200

    fixup! test: make test-worker-prof more robust

--- test/sequential/test-worker-prof.js
+++ test/sequential/test-worker-prof.js
@@ -18,7 +18,7 @@ if (process.argv[2] === 'child') {
 
   const { Worker } = require('worker_threads');
   const w = new Worker(fixtures.path('workload', 'spin.js'), {
-    env: { NODE_TEST_SPIN_MS: 100 }
+    env: { NODE_TEST_SPIN_MS: 1000 }
   });
 
   w.on('exit', common.mustCall(() => {
@@ -58,8 +58,8 @@ if (process.argv[2] === 'child') {
   console.log('parent ticks', parentTicks.length);
   // When not tracking Worker threads, only 1 or 2 ticks would
   // have been recorded.
-  // prof_sampling_interval is by default 1 millsecond, so spinning for 100
-  // milliseconds should result in ~100 ticks.
+  // prof_sampling_interval is by default 1 millsecond. A higher NODE_TEST_SPIN_MS
+  // should result in more ticks, while 15 should be safe on most machines.
   assert(workerTicks.length > 15, `worker ticks <= 15:\n${workerTicks.join('\n')}`);
   assert(parentTicks.length > 15, `parent ticks <= 15:\n${parentTicks.join('\n')}`);
 }
