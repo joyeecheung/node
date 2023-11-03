871b471c130f9dff7cc07c5da5a768ed2ff113da
Joyee Cheung <joyeec9h3@gmail.com>
2023-11-03 14:49:09 +0000

    src: manage DirHandle via cppgc

--- src/node_dir.h
+++ src/node_dir.h
@@ -3,38 +3,44 @@
 
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
+#include "cppgc/garbage-collected.h"
 #include "node_file.h"
+#include "v8-cppgc.h"
 
 namespace node {
 
 namespace fs_dir {
 
 // Needed to propagate `uv_dir_t`.
-class DirHandle : public BaseObject {
+class DirHandle final : public cppgc::GarbageCollected<DirHandle>,
+                        public CppgcMixin,
+                        public MemoryRetainer {
  public:
   static DirHandle* New(Environment* env, uv_dir_t* dir);
   ~DirHandle() override;
 
-  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Read(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Close(const v8::FunctionCallbackInfo<v8::Value>& args);
 
   inline uv_dir_t* dir() { return dir_; }
-
   void MemoryInfo(MemoryTracker* tracker) const override;
   SET_MEMORY_INFO_NAME(DirHandle)
   SET_SELF_SIZE(DirHandle)
 
+  DirHandle(Environment* env, v8::Local<v8::Object> obj, uv_dir_t* dir);
   DirHandle(const DirHandle&) = delete;
   DirHandle& operator=(const DirHandle&) = delete;
   DirHandle(const DirHandle&&) = delete;
   DirHandle& operator=(const DirHandle&&) = delete;
 
- private:
-  DirHandle(Environment* env, v8::Local<v8::Object> obj, uv_dir_t* dir);
+  CPPGC_MIXIN_METHODS()
+  DEFAULT_CPPGC_TRACE()
 
+ private:
   // Synchronous close that emits a warning
   void GCClose();
+
+  CPPGC_MIXIN_FIELDS()
 
   uv_dir_t* dir_;
   // Multiple entries are read through a single libuv call.
