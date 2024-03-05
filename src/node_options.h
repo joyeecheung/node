38c74d3114c71281aa73db82357a634052b626fa
Joyee Cheung <joyeec9h3@gmail.com>
2024-03-05 17:05:51 +0100

    src: add --print-pending-tla flag

--- src/node_options.h
+++ src/node_options.h
@@ -105,6 +105,7 @@ class EnvironmentOptions : public Options {
   bool abort_on_uncaught_exception = false;
   std::vector<std::string> conditions;
   bool detect_module = false;
+  bool print_pending_tla = false;
   std::string dns_result_order;
   bool enable_source_maps = false;
   bool experimental_fetch = true;
