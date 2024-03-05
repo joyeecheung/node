d642032e333e366915d4e493973ca5c4092b6d2b
Joyee Cheung <joyeec9h3@gmail.com>
2024-03-05 17:05:51 +0100

    module: support require()ing synchronous ESM graphs
    This patch adds `require()` support for synchronous ESM graphs under
    the flag --experimental-require-module.
    
    This is based on the the following design aspect of ESM:
    
    - The resolution can be synchronous (up to the host)
    - The evaluation of a synchronous graph (without top-level await)
      is also synchronous, and, by the time the module graph is
      instantiated (before evaluation starts), this is is already known.
    
    When --experimental-require-module is enabled, and require() resolves
    to an ES module, if the module is fully synchronous (contains no
    top-level await), the `require()` call returns the module name space
    object. Otherwise, Node.js throws an error without executing the module.
    If `--print-required-tla` is passed, Node.js will evaluate the module,
    try to locate the top-level awaits, and print their location to help
    users fix them.
    
    With this option alone, the module being `require()`'d should be explicitly
    marked as an ES module either using the `"type": "module"` field in
    `package.json` or the `.mjs` extension. To load implicit ES modules ending
    with `.js` using automatic syntax-based module type detection, use
    `--experimental-require-module-with-detection`.

--- doc/api/cli.md
+++ doc/api/cli.md
@@ -871,6 +871,43 @@ added: v11.8.0
 
 Use the specified file as a security policy.
 
+### `--experimental-require-module`
+
+<!-- YAML
+added: REPLACEME
+-->
+
+> Stability: 1.1 - Active Developement
+
+Supports loading a synchronous ES module graph in `require()`.
+
+When `--experimental-require-module` is enabled, and `require()`
+resolves to an ES module, if the module is fully synchronous
+(contains no top-level await), the `require()` call returns the
+module name space object. Otherwise, Node.js throws an error
+without executing the module. If `--experimental-print-required-tla`
+is passed, Node.js will evaluate the module, try to locate the
+top-level awaits, and print their location to help users fix them.
+
+With this option alone, the module being `require()`'d should be
+explicitly marked as an ES module either using the `"type": "module"`
+field in `package.json` or the `.mjs` extension. To load implicit ES
+modules ending with `.js` using automatic syntax-based module type
+detection, use `--experimental-require-module-with-detection`.
+
+### `--experimental-require-module-with-detection`
+
+<!-- YAML
+added: REPLACEME
+-->
+
+> Stability: 1.1 - Active Developement
+
+In addition to what `--experimental-require-module` supports, when the module
+being `require()`'d is not explicitly marked as an ES Module using the
+`"type": "module"` field in `package.json` or the `.mjs` extension, it will
+try to detect module type based on the syntax of the module.
+
 ### `--experimental-sea-config`
 
 <!-- YAML
@@ -1577,6 +1614,18 @@ changes:
 -->
 
 Identical to `-e` but prints the result.
+
+### `--experimental-print-required-tla`
+
+<!-- YAML
+added: REPLACEME
+-->
+
+This flag is only useful when `--experimental-require-module` is enabled.
+
+If the ES module being `require()`'d contains top-level await, this flag
+allows Node.js to evaluate the module, try to locate the
+top-level awaits, and print their location to help users fix them.
 
 ### `--prof`
 
@@ -2512,6 +2561,9 @@ Node.js options that are allowed are:
 * `--experimental-network-imports`
 * `--experimental-permission`
 * `--experimental-policy`
+* `--experimental-print-required-tla`
+* `--experimental-require-module-with-detection`
+* `--experimental-require-module`
 * `--experimental-shadow-realm`
 * `--experimental-specifier-resolution`
 * `--experimental-top-level-await`
--- src/node_contextify.cc
+++ src/node_contextify.cc
@@ -28,6 +28,7 @@
 #include "node_errors.h"
 #include "node_external_reference.h"
 #include "node_internals.h"
+#include "node_process-inl.h"
 #include "node_sea.h"
 #include "node_snapshot_builder.h"
 #include "node_watchdog.h"
@@ -56,6 +57,7 @@ using v8::Maybe;
 using v8::MaybeLocal;
 using v8::MeasureMemoryExecution;
 using v8::MeasureMemoryMode;
+using v8::Message;
 using v8::MicrotaskQueue;
 using v8::MicrotasksPolicy;
 using v8::Name;
@@ -1409,6 +1411,24 @@ constexpr std::array<std::string_view, 3> esm_syntax_error_messages = {
     "Unexpected token 'export'",                     // `export` statements
     "Cannot use 'import.meta' outside a module"};    // `import.meta` references
 
+// TODO(joyeecheung): see if we can upstream an API to V8 for this.
+bool FindMessage(Isolate* isolate,
+                 Local<Message> error_message,
+                 const std::array<std::string_view, 3>& allow_list) {
+  if (error_message.IsEmpty()) {
+    return false;
+  }
+  Utf8Value message_utf8(isolate, error_message->Get());
+  auto message = message_utf8.ToStringView();
+
+  for (const auto& error_message : allow_list) {
+    if (message.find(error_message) != std::string_view::npos) {
+      return true;
+    }
+  }
+  return false;
+}
+
 void ContextifyContext::ContainsModuleSyntax(
     const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
@@ -1478,25 +1498,34 @@ void ContextifyContext::ContainsModuleSyntax(
 
   bool found_error_message_caused_by_module_syntax = false;
   if (try_catch.HasCaught() && !try_catch.HasTerminated()) {
-    Utf8Value message_value(env->isolate(), try_catch.Message()->Get());
-    auto message = message_value.ToStringView();
-
-    for (const auto& error_message : esm_syntax_error_messages) {
-      if (message.find(error_message) != std::string_view::npos) {
-        found_error_message_caused_by_module_syntax = true;
-        break;
-      }
-    }
+    found_error_message_caused_by_module_syntax = FindMessage(
+        env->isolate(), try_catch.Message(), esm_syntax_error_messages);
   }
   args.GetReturnValue().Set(found_error_message_caused_by_module_syntax);
 }
 
+const char* require_esm_warning =
+    "To load an ES module, use --experimental-require-module if you want to"
+    "require() it and it contains no top-level await.\n"
+    "If it's import'ed, set \"type\": \"module\" in the package.json, "
+    "or use the .mjs extension.";
+
+const char* require_esm_warning_without_detection =
+    "The module being require()d looks like an ES module, but it is not "
+    "explicitly marked with \"type\": \"module\" in the package.json or "
+    "with a .mjs extention. To enable automatic detection of module syntax "
+    "in require(), use --experimental-require-module-with-detection.";
+
+static bool warned_about_require_esm = false;
+static bool warned_about_require_esm_detection = false;
 static void CompileFunctionForCJSLoader(
     const FunctionCallbackInfo<Value>& args) {
   CHECK(args[0]->IsString());
   CHECK(args[1]->IsString());
+
   Local<String> code = args[0].As<String>();
   Local<String> filename = args[1].As<String>();
+
   Isolate* isolate = args.GetIsolate();
   Local<Context> context = isolate->GetCurrentContext();
   Environment* env = Environment::GetCurrent(context);
@@ -1532,32 +1561,80 @@ static void CompileFunctionForCJSLoader(
 #endif
   ScriptCompiler::Source source(code, origin, cached_data);
 
-  TryCatchScope try_catch(env);
-
   std::vector<Local<String>> params = GetCJSParameters(env->isolate_data());
 
-  MaybeLocal<Function> maybe_fn = ScriptCompiler::CompileFunction(
-      context,
-      &source,
-      params.size(),
-      params.data(),
-      0,       /* context extensions size */
-      nullptr, /* context extensions data */
-      // TODO(joyeecheung): allow optional eager compilation.
-      cached_data == nullptr ? ScriptCompiler::kNoCompileOptions
-                             : ScriptCompiler::kConsumeCodeCache,
-      v8::ScriptCompiler::NoCacheReason::kNoCacheNoReason);
-
+  bool retry_as_esm = false;
   Local<Function> fn;
-  if (!maybe_fn.ToLocal(&fn)) {
-    if (try_catch.HasCaught() && !try_catch.HasTerminated()) {
-      errors::DecorateErrorStack(env, try_catch);
-      if (!try_catch.HasTerminated()) {
-        try_catch.ReThrow();
+
+  {
+    TryCatchScope try_catch(env);
+    MaybeLocal<Function> maybe_fn;
+    {
+      ShouldNotAbortOnUncaughtScope should_not_abort(env);
+      maybe_fn = ScriptCompiler::CompileFunction(
+          context,
+          &source,
+          params.size(),
+          params.data(),
+          0,       /* context extensions size */
+          nullptr, /* context extensions data */
+          // TODO(joyeecheung): allow optional eager compilation.
+          cached_data == nullptr ? ScriptCompiler::kNoCompileOptions
+                                 : ScriptCompiler::kConsumeCodeCache,
+          v8::ScriptCompiler::NoCacheReason::kNoCacheNoReason);
+    }
+
+    if (!maybe_fn.ToLocal(&fn)) {
+      if (try_catch.HasCaught()) {
+        Utf8Value filename_utf8(isolate, filename);
+        size_t flen = filename_utf8.length();
+        if (flen < 4 ||
+            filename_utf8.ToString().substr(flen - 4, flen) == ".cjs") {
+          USE(ProcessEmitWarningSync(env, require_esm_warning));
+          errors::DecorateErrorStack(env, try_catch);
+          try_catch.ReThrow();
+          return;
+        }
+
+        Local<Value> error = try_catch.Exception();
+        Local<Message> message = try_catch.Message();
+        if (!error->IsNativeError() || message.IsEmpty() ||
+            !FindMessage(isolate, message, esm_syntax_error_messages)) {
+          errors::DecorateErrorStack(env, try_catch);
+          try_catch.ReThrow();
+          return;
+        }
+
+        if (!env->options()->require_module) {
+          if (!warned_about_require_esm) {
+            USE(ProcessEmitWarningSync(env, require_esm_warning));
+            warned_about_require_esm = true;
+          }
+          errors::DecorateErrorStack(env, try_catch);
+          try_catch.ReThrow();
+          return;
+        }
+
+        if (!env->options()->require_module_with_detection) {
+          if (!warned_about_require_esm_detection) {
+            USE(ProcessEmitWarningSync(env,
+                                       require_esm_warning_without_detection));
+            warned_about_require_esm_detection = true;
+          }
+          errors::DecorateErrorStack(env, try_catch);
+          try_catch.ReThrow();
+          return;
+        }
+
+        // The file being compiled is likely ESM.
+        retry_as_esm = true;
       }
-      return;
     }
   }
+
+  // TODO(joyeecheung): refactor the ESM compilation so that we can reparse
+  // as ESM here to confirm it's ESM, and pass the compiled module into the
+  // ModuleWrap to save the subsequent compilation.
 
   bool cache_rejected = false;
 #ifndef DISABLE_SINGLE_EXECUTABLE_APPLICATION
@@ -1570,11 +1647,14 @@ static void CompileFunctionForCJSLoader(
       env->cached_data_rejected_string(),
       env->source_map_url_string(),
       env->function_string(),
+      FIXED_ONE_BYTE_STRING(isolate, "retryAsESM"),
   };
   std::vector<Local<Value>> values = {
       Boolean::New(isolate, cache_rejected),
-      fn->GetScriptOrigin().SourceMapUrl(),
-      fn,
+      retry_as_esm ? v8::Undefined(isolate).As<Value>()
+                   : fn->GetScriptOrigin().SourceMapUrl(),
+      retry_as_esm ? v8::Undefined(isolate).As<Value>() : fn.As<Value>(),
+      Boolean::New(isolate, retry_as_esm),
   };
   Local<Object> result = Object::New(
       isolate, v8::Null(isolate), names.data(), values.data(), names.size());
