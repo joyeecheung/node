8bc99eb39c32793f9baa0afaedac5fe9bfc48159
Joyee Cheung <joyeec9h3@gmail.com>
2024-03-08 18:28:37 +0100

    module: support require()ing synchronous ESM graphs
    This patch adds `require()` support for synchronous ESM graphs under
    the flag --experimental-require-module.
    
    This is based on the the following design aspect of ESM:
    
    - The resolution can be synchronous (up to the host)
    - The evaluation of a synchronous graph (without top-level await)
      is also synchronous, and, by the time the module graph is
      instantiated (before evaluation starts), this is is already known.
    
    When the module being require()ed has .mjs extension or there are
    other explicit indicators that it's an ES module, we load it as an
    ES module. If the graph is synchronous, we return the module namespace
    as the exports. If the graph contains top-level await, we throw an
    error before evaluating the module. If an additional flag
    --print-pending-tla is passed, we proceeds to evaluation but do not
    run the microtasks, only to find out where the TLA is and print
    their location to help users fix them.
    
    If there are not explicit indicators whether a .js file is CJS or ESM,
    we parse it as CJS first. If the parse error indicates that it contains
    ESM syntax, we parse it again as ESM. If the second parsing succeeds,
    we continue to treat it as ESM.

--- src/module_wrap.h
+++ src/module_wrap.h
@@ -79,6 +79,9 @@ class ModuleWrap : public BaseObject {
   ~ModuleWrap() override;
 
   static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void LinkSync(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void InstantiateSync(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void EvaluateSync(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Link(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Instantiate(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Evaluate(const v8::FunctionCallbackInfo<v8::Value>& args);
