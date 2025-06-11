4ff733a94d5d9775d779791ce6fe2cf4b3bf2e44
Joyee Cheung <joyeec9h3@gmail.com>
2025-06-11 17:43:46 +0200

    implement timeout

--- test/parallel/test-https-proxy-request-tunnel-timeout.js
+++ test/parallel/test-https-proxy-request-tunnel-timeout.js
@@ -36,12 +36,13 @@ const { runProxiedRequest } = require('../common/proxy-server');
   const { code, signal, stderr, stdout } = await runProxiedRequest({
     NODE_USE_ENV_PROXY: 1,
     REQUEST_URL: requestUrl,
+    REQUEST_TIMEOUT: 1000,
     HTTPS_PROXY: `http://localhost:${proxy.address().port}`,
     NODE_EXTRA_CA_CERTS: fixtures.path('keys', 'fake-startcom-root-cert.pem'),
   });
 
-  // The proxy client should get a connection timeout or close error.
-  assert(stderr.includes('ERR_PROXY_ERROR') || stderr.includes('ECONNRESET') || stderr.includes('ended unexpectedly'));
+  // The proxy client should get a connection timeout.
+  assert.match(stderr, /Request timed out/);
   assert.strictEqual(stdout.trim(), '');
   assert.strictEqual(code, 0);
   assert.strictEqual(signal, null);
