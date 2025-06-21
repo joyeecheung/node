29a92024199cdcb1996b39a10f07e9a9f3a5fca1
Joyee Cheung <joyeec9h3@gmail.com>
2025-06-21 10:16:02 +0200

    move tunnel code to https agent

--- test/client-proxy/test-https-proxy-request-tunnel-timeout-agent.mjs
+++ test/client-proxy/test-https-proxy-request-tunnel-timeout-agent.mjs
@@ -42,6 +42,7 @@ const { code, signal, stderr, stdout } = await runProxiedRequest({
 
 // The proxy client should get a connection timeout.
 assert.match(stderr, /Request timed out/);
+assert.match(stderr, /timed out after 1000ms/);
 assert.strictEqual(stdout.trim(), '');
 assert.strictEqual(code, 0);
 assert.strictEqual(signal, null);
