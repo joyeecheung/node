19d38ea9d96fa9d64435453847c092e32cf9c915
Joyee Cheung <joyeec9h3@gmail.com>
2025-08-19 15:56:28 +0200

    test: split wasi-preview-1.js

--- test/wasi/test-wasi-pthread.js
+++ test/wasi/test-wasi-pthread.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Tests test/wasi/wasm/pthread.wasm, see test/wasi/c/pthread.c
-require('../common');
-const { testWasiPreview1 } = require('../common/wasi');
+const common = require('../common');
+const run = require('../fixtures/wasm32-wasip1-threads.js');
 
-testWasiPreview1(['--target=wasm32-wasip1-threads', 'pthread']);
+run('pthread').then(common.mustCall());
