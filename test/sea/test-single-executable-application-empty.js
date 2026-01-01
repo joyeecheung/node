865d4bcf57d12ca5c4336ec0637b55fa2dc67ea7
Joyee Cheung <joyeec9h3@gmail.com>
2026-01-01 15:37:58 +0100

    test: use fixture directories for sea tests
    Instead of copying and writing files on the fly for SEA tests,
    put the fixtures into a directory and copy them into tmpdir
    for testing. This allows easier reproduction and debugging
    when they do fail - we can just copy the entire fixture directory
    and test directly from there.

--- test/sea/test-single-executable-application-empty.js
+++ test/sea/test-single-executable-application-empty.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const common = require('../common');
-
+require('../common');
 const {
   generateSEA,
   skipIfSingleExecutableIsNotSupported,
@@ -13,45 +12,17 @@ skipIfSingleExecutableIsNotSupported();
 // script.
 
 const tmpdir = require('../common/tmpdir');
-const { writeFileSync, existsSync } = require('fs');
+const fixtures = require('../common/fixtures');
 const { spawnSyncAndExitWithoutError } = require('../common/child_process');
-const assert = require('assert');
-
-const configFile = tmpdir.resolve('sea-config.json');
-const seaPrepBlob = tmpdir.resolve('sea-prep.blob');
-const outputFile = tmpdir.resolve(process.platform === 'win32' ? 'sea.exe' : 'sea');
 
 tmpdir.refresh();
 
-writeFileSync(tmpdir.resolve('empty.js'), '', 'utf-8');
-writeFileSync(configFile, `
-{
-  "main": "empty.js",
-  "output": "sea-prep.blob"
-}
-`);
-
-spawnSyncAndExitWithoutError(
-  process.execPath,
-  ['--experimental-sea-config', 'sea-config.json'],
-  { cwd: tmpdir.path });
-
-assert(existsSync(seaPrepBlob));
-
-// Verify the workflow.
-try {
-  generateSEA(outputFile, process.execPath, seaPrepBlob, true);
-} catch (e) {
-  if (/Cannot copy/.test(e.message)) {
-    common.skip(e.message);
-  } else if (common.isWindows) {
-    if (/Cannot sign/.test(e.message) || /Cannot find signtool/.test(e.message)) {
-      common.skip(e.message);
-    }
-  }
-
-  throw e;
-}
+const outputFile = generateSEA(
+  fixtures.path('sea', 'empty'),
+  tmpdir.path,
+  'sea-config.json',
+  true,
+);
 
 spawnSyncAndExitWithoutError(
   outputFile,
